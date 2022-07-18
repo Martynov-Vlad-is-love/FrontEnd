9ff79b3572d6e9fdbe5644e1fb02b602b5fde2b6
Martynov-Vlad-is-love <vladislav.martynov.0@gmail.com>
2022-07-18 20:44:09 +0300

    .

---
+++ diving/lib/generated/codegen_loader.g.dart
@@ -0,0 +1,314 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> en = {
+  "you_should_authorize_to_use_this_web_site": "You should authorize to use this web-site",
+  "authorization": "Authorization",
+  "login": "Login",
+  "password": "Password",
+  "registration": "Registration",
+  "wrong_login_or_password": "Wrong login or password",
+  "authorize": "Authorize",
+  "enter_your_login": "Enter your login",
+  "enter_your_password": "Enter your password",
+  "enter_your_name": "Enter your name",
+  "enter_your_surname": "Enter your surname",
+  "enter_your_age": "Enter your age",
+  "do_you_have_an_account": "Do you have an account?",
+  "data_cant_be_empty_or_less_than_3_liters": "Data can't be empty or less than 3 liters",
+  "register": "Register",
+  "do_you_really_want_to_leave": "Do you really want to leave?",
+  "yes": "Yes",
+  "no": "No",
+  "courses": "Courses",
+  "profile": "Profile",
+  "promo_codes": "Promo codes",
+  "about_us": "About us",
+  "users": "Users",
+  "user_courses": "User courses",
+  "loading": "Loading...",
+  "create_new_course": "Create new course",
+  "do_you_really_want_to_delete_this_course": "Do you really want to delete this course?",
+  "delete": "Delete",
+  "cancel": "Cancel",
+  "delete_course_by_id": "Delete course by id",
+  "course_name": "Course name",
+  "course_cost": "Course cost",
+  "description": "Description",
+  "minimal_hours_under_water": "Minimal hours under water",
+  "creating_new_course": "Creating new course",
+  "image_url": "Image url",
+  "set_image": "Set image",
+  "data_cant_be_empty_or_less_than_2_liters": "Data can't be empty or less than 2 liters",
+  "wrong_input_data": "Wrong input data",
+  "success": "Success",
+  "create": "Create",
+  "creating_new_promo_code": "Creating new promo code",
+  "creating_new_record": "Creating new record",
+  "some_data_is_wrong": "Some data is wrong",
+  "creating_new_user": "Creating new user",
+  "passwords_should_consist_more_than_3_symbols": "Passwords should consist more than 3 symbols",
+  "promo_code": "Promo code",
+  "discount": "Discount",
+  "is_active": "Is active",
+  "user_id": "User id",
+  "course_id": "Course id",
+  "available": "Available",
+  "promo_code_id": "Promo code id",
+  "completed": "Completed",
+  "total_price": "Total price",
+  "max_depth": "Max depth",
+  "phone_number": "Phone number",
+  "role_id_help": "Role Id (0 = User| 2 = Admin)",
+  "disease": "Disease",
+  "hours_under_water": "Hours under water",
+  "name": "Name",
+  "surname": "Surname",
+  "age": "Age",
+  "save": "Save",
+  "course_not_found": "Course not found",
+  "promo_code_not_found": "Promo code not found",
+  "user_course_not_found": "User course not found",
+  "create_new_record": "Create new record",
+  "create_new_promo_code": "Create new promo code",
+  "do_you_really_want_to_delete_this_promo_code": "Do you really want to delete this promo code?",
+  "delete_promo_code_by_id": "Delete promo code by id",
+  "do_you_really_want_to_delete_this_record": "Do you really want to delete this record?",
+  "delete_record_by_id": "Delete record by id",
+  "create_new_user": "Create new user",
+  "do_you_really_want_to_delete_this_user": "Do you really want to delete this user?",
+  "sorry_but_your_hours_under_water_is_lower_than_requires": "Sorry, but your hours under water is lower than requires",
+  "do_you_want_to_buy_this_course": "Do you want to buy this course?",
+  "confirm": "Confirm",
+  "repeat_password": "Repeat Password",
+  "delete_user_by_id": "Delete user by id",
+  "buy_this_course": "Buy this course",
+  "purchased_courses": "Purchased courses",
+  "you_already_complete_this_course_do_you_want_to_take_certificate": "You already complete this course. Do you want to take certificate?",
+  "course_bought": "Course bought",
+  "your_profile": "Your profile",
+  "passwords_should_be_equal": "Passwords should be equal",
+  "user_not_found": "User not found",
+  "dollars": "dollars",
+  "your_hours_under_water_": {
+    "other": "Your hours under water: {}"
+  },
+  "completed_": {
+    "other": "Completed on {}%"
+  },
+  "viewing_course_": {
+    "other": "Viewing course: {courseName}"
+  },
+  "editing_course_": {
+    "other": "Editing course {}"
+  },
+  "editing_promo_code_": {
+    "other": "Editing promo code {}"
+  },
+  "editing_record_": {
+    "other": "Editing record {}"
+  },
+  "editing_user_": {
+    "other": "Editing user {}"
+  },
+  "id_": {
+    "other": "Id: {}"
+  },
+  "promo_code_": {
+    "other": "Promo code: {promoCode}"
+  },
+  "discount_": {
+    "other": "Discount: {}"
+  },
+  "is_active_": {
+    "other": "Is active: {isActive}"
+  },
+  "course_id_": {
+    "other": "Course id: {}"
+  },
+  "user_id_": {
+    "other": "User id: {}"
+  },
+  "total_price_": {
+    "other": "Total price: {}"
+  },
+  "login_": {
+    "other": "Login: {login}"
+  },
+  "role_id_": {
+    "other": "Role: id {}"
+  },
+  "course_name_": {
+    "other": "Course name {courseName}"
+  },
+  "min_hours_under_water_": {
+    "other": "Minimum hours under water: {}"
+  },
+  "cost_": {
+    "other": "Cost: {}"
+  },
+  "about_us_page": "Hello, my name is Vladislav, I'm a student at the Kharkov National University of Radio Electronics, this project is my course work. My e-mail: vladyslav.martynov1@nure.ua."
+};
+static const Map<String,dynamic> uk = {
+  "you_should_authorize_to_use_this_web_site": "Щоб скористатися сайтом Вам потрібно авторизоватися",
+  "authorization": "Авторизація",
+  "login": "Логін",
+  "password": "Пароль",
+  "registration": "Реєстрація",
+  "wrong_login_or_password": "Невірний логін або пароль",
+  "authorize": "Авторизуватися",
+  "enter_your_login": "Введіть логін",
+  "enter_your_password": "Введіть пароль",
+  "enter_your_name": "Введіть ім'я",
+  "enter_your_surname": "Введіть прізвище",
+  "enter_your_age": "Введіть ваш вік",
+  "do_you_have_an_account": "Вже маєте аккаунт?",
+  "data_cant_be_empty_or_less_than_3_liters": "Данні не можуть бути порожніми, або меншими за 3 символи",
+  "register": "Зареєструватися",
+  "do_you_really_want_to_leave": "Ви дійсно хочете вийти?",
+  "yes": "Так",
+  "no": "Ні",
+  "courses": "Курси",
+  "profile": "Профіль",
+  "promo_codes": "Промо коди",
+  "about_us": "Про нас",
+  "users": "Користувачі",
+  "user_courses": "Курси користувачів",
+  "loading": "Завантаження...",
+  "create_new_course": "Створити новий курс",
+  "do_you_really_want_to_delete_this_course": "Ви дійсно хочете видалити цей курс?",
+  "delete": "Видалити",
+  "cancel": "Скасувати",
+  "delete_course_by_id": "Видалити курс по Id",
+  "course_name": "Назва курсу",
+  "course_cost": "Ціна курсу",
+  "description": "Опис",
+  "minimal_hours_under_water": "Мінімум годин під водою",
+  "creating_new_course": "Створення нового курсу",
+  "image_url": "Посилання на картинку",
+  "set_image": "Встановити картинку",
+  "data_cant_be_empty_or_less_than_2_liters": "Дані не можуть бути менше ніж 2 символи",
+  "wrong_input_data": "Неправильні вхідні дані",
+  "success": "Успіх",
+  "create": "Створити",
+  "creating_new_promo_code": "Створити новий промо код",
+  "creating_new_record": "Створити новий запис",
+  "some_data_is_wrong": "Якісь дані були введені невірно",
+  "creating_new_user": "Створення нового користувача",
+  "passwords_should_consist_more_than_3_symbols": "Пароль повинен містити більше 3х символів",
+  "promo_code": "Промо код",
+  "discount": "Знижка",
+  "is_active": "Чи активний",
+  "user_id": "Id користувача",
+  "course_id": "Id курсу",
+  "available": "Чи доступний",
+  "promo_code_id": "Id промо коду",
+  "completed": "Виконаний на",
+  "total_price": "Кінцева ціна",
+  "max_depth": "Максимальна глибина",
+  "phone_number": "Номер телефону",
+  "role_id_help": "Id ролі (0 = Користувач | 2 = Адмін)",
+  "disease": "Захворювання",
+  "hours_under_water": "Годин під водою",
+  "name": "Ім'я",
+  "surname": "Прізвище",
+  "age": "Вік",
+  "save": "Зберегти",
+  "course_not_found": "Курс не знайдений",
+  "promo_code_not_found": "Промо код не знайдений",
+  "user_course_not_found": "Курс користувача не знайдений",
+  "create_new_record": "Створити новий запис",
+  "create_new_promo_code": "Створити новий промо код",
+  "do_you_really_want_to_delete_this_promo_code": "Ви дійсно хочете видалити цей промо код?",
+  "delete_promo_code_by_id": "Видалити промокод по id",
+  "do_you_really_want_to_delete_this_record": "Ви дійсно хочете видалити цей запис?",
+  "delete_record_by_id": "Видалити запис по id",
+  "create_new_user": "Створити нового користувача",
+  "do_you_really_want_to_delete_this_user": "Ви дійсно хочете видалити цього користувача?",
+  "sorry_but_your_hours_under_water_is_lower_than_requires": "Вибачте, але вашого часу під водою менше ніж потрібно",
+  "do_you_want_to_buy_this_course": "Ви хочете купити цей курс?",
+  "confirm": "Підтвердити",
+  "repeat_password": "Повторіть пароль",
+  "delete_user_by_id": "Видалити користувача по id",
+  "buy_this_course": "Купити цей курс",
+  "purchased_courses": "Куплені курси",
+  "you_already_complete_this_course_do_you_want_to_take_certificate": "Ви вже закінчили цей курс, хочете отримати сертифікат?",
+  "course_bought": "Курс куплений",
+  "your_profile": "Ваш профіль",
+  "passwords_should_be_equal": "Паролі повинні бути однакові",
+  "user_not_found": "Користувач не знайдений",
+  "dollars": "долларів",
+  "your_hours_under_water_": {
+    "other": "Ваші години під водою: {}"
+  },
+  "cost_": {
+    "other": "Вартість: {}"
+  },
+  "completed_": {
+    "other": "Закінчений на {}%"
+  },
+  "viewing_course_": {
+    "other": "Перегляд курсу: {courseName}"
+  },
+  "editing_course_": {
+    "other": "Редагування курсу {}"
+  },
+  "editing_user_": {
+    "other": "Редагування користувача {}"
+  },
+  "editing_promo_code_": {
+    "other": "Редагування промо коду {}"
+  },
+  "editing_record_": {
+    "other": "Редагування запису {}"
+  },
+  "id_": {
+    "other": "Id: {}"
+  },
+  "promo_code_": {
+    "other": "Промо код: {promoCode}"
+  },
+  "discount_": {
+    "other": "Знижка: {}"
+  },
+  "is_active_": {
+    "other": "Чи активний: {isActive}"
+  },
+  "course_id_": {
+    "other": "Id курсу: {}"
+  },
+  "user_id_": {
+    "other": "Id користувача: {}"
+  },
+  "total_price_": {
+    "other": "Кінцева ціна: {}"
+  },
+  "login_": {
+    "other": "Логін: {login}"
+  },
+  "role_id_": {
+    "other": "Id ролі: {}"
+  },
+  "course_name_": {
+    "other": "Назва курсу {courseName}"
+  },
+  "min_hours_under_water_": {
+    "other": "Мінімум годин под водою: {}"
+  },
+  "about_us_page": "         Привіт, мене звуть Владислав, я студент Харківського Національного Університету Радіоелектроніки, цей проект є моєю курсовою роботою. Мій email: vladyslav.martynov1@nure.ua."
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"en": en, "uk": uk};
+}
